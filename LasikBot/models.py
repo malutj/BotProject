be94511df9b7e582b81ea8a10433f92c748051a3
malutj <jmalutich@gmail.com>
2016-07-19 02:13:25 +0000

    Updated models

--- LasikBot/models.py
+++ LasikBot/models.py
@@ -9,22 +9,21 @@ class FacebookUser ( models.Model ):
     phoneNumber = models.CharField ( max_length = 10, null = True, blank = True, unique = False )
 
 
-class Lead ( models.Model ):
-    facebookUser = models.ForeignKey ( FacebookUser, null = False, blank = False, on_delete = models.CASCADE( ) )
-    practice = models.ForeignKey ( Client, null = False, blank = False, on_delete = models.CASCADE( ) )
-    dateReceived = models.DateField ( auto_now_add = True, null = False, blank = False )
-
-
-
 class Client ( models.Model ):
     facebookPageId = models.CharField ( max_length = 100, null = False, blank = False, unique = True )
     # todo should I make the practice name field unique?
     practiceName = models.CharField ( max_length = 100, null = False, blank = False, unique = False )
     clientEmail = models.EmailField ( max_length = 255, null = False, blank = False, unique = False )
 
+    
+class Lead ( models.Model ):
+    facebookUser = models.ForeignKey ( FacebookUser, null = False, blank = False, on_delete = models.CASCADE )
+    practice = models.ForeignKey ( Client, null = False, blank = False, on_delete = models.CASCADE )
+    dateReceived = models.DateField ( auto_now_add = True, null = False, blank = False )
+
 
 class Availability ( models.Model ):
-    practice = models.ForeignKey ( Client, null = False, blank = False, on_delete = models.CASCADE( ) )
+    practice = models.ForeignKey ( Client, null = False, blank = False, on_delete = models.CASCADE )
     dayOfTheWeek = models.CharField ( max_length = 10, null = False, blank = False, unique = False )
     startTime = models.TimeField ( null = False, blank = False, unique = False )
     endTime = models.TimeField ( null = False, blank = False, unique = False )
