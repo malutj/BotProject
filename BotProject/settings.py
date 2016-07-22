72255aeff080e6fb9c5fb66b9c8835acb908a108
Jason Malutich <jmalutich@gmail.com>
2016-07-21 20:02:32 -0500

    trying to get models to load into DB

--- BotProject/settings.py
+++ BotProject/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'LasikBot.apps.LasikbotConfig'
+    'LasikBot.apps.LasikbotConfig',
+    'LasikBot'
 ]
 
 MIDDLEWARE_CLASSES = [
