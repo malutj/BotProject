cbcf58db71d6b829a1edc5721857b0e97bf270f9
Jason Malutich <jmalutich@gmail.com>
2016-07-23 21:52:29 -0500

    add availability table to admin page

--- LasikBot/admin.py
+++ LasikBot/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from LasikBot import models
 
 # Register your models here.
-admin.site.register( models.client )
-admin.site.register( models.lead )
-admin.site.register( models.facebookuser )
+admin.site.register(models.client)
+admin.site.register(models.lead)
+admin.site.register(models.facebookuser)
+admin.site.register(models.availability)
 
