a669b9d9bfabcf4df70cbaab34397ef84c4ab300
Jason Malutich <jmalutich@gmail.com>
2016-07-24 12:19:47 -0500

    logging for bugs

--- LasikBot/FacebookComm.py
+++ LasikBot/FacebookComm.py
@@ -357,8 +357,12 @@ class FacebookComm:
             self.send_buttons(facebook_data.facebook_id, message, appt_option_payload)
         else:
             print("No appointment options.")
-            message = "Sorry, it doesn't look as though ",\
-                      client.objects.get(facebook_page_id = facebook_data.page_id).practice_name, \
+            # message = "Sorry, it doesn't look as though ",\
+            #           client.objects.get(facebook_page_id = facebook_data.page_id).practice_name, \
+            #           " has any consultation times available. I will talk with them and have them " \
+            #           "reach out to you personally to schedule something. Thanks!"
+
+            message = "Sorry, it doesn't look as though <PRACTICE NAME>", \
                       " has any consultation times available. I will talk with them and have them " \
                       "reach out to you personally to schedule something. Thanks!"
 
